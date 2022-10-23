f866745b0c457db0931731771752f9fed66dd5a7
Precious <precious.akam@hotmail.com>
2022-10-23 10:37:30 +1030

    Changes

--- website/views.py
+++ website/views.py
@@ -93,7 +93,9 @@ def approval():
 def profile():
     form = EditProfileForm()
     #Get subscription for when school is is the same as current user school id
-    subscription = Subscription.query.filter(Subscription.school_id==current_user.school_id).first()
+    sub = Subscription.query.with_entities(Subscription.expiry_date).filter(Subscription.school_id==current_user.school_id).first()
+    #Get the strftime of the subscription
+    subscription = sub.strftime('%Y-%m-%d') 
     if form.validate_on_submit():
         if form.picture.data:
             picture_file = save_picture(form.picture.data)
