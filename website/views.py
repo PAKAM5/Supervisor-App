c1931992451670c92a2eb752ffa28e4b696f0e7c
Precious <precious.akam@hotmail.com>
2022-10-23 10:53:25 +1030

    Changes

--- website/views.py
+++ website/views.py
@@ -93,7 +93,7 @@ def approval():
 def profile():
     form = EditProfileForm()
     #Get subscription for when school is is the same as current user school id
-    subscription = Subscription.query.with_entities(Subscription.expiry_date).filter_by(Subscription.school_id==current_user.school_id).first()
+    subscription = Subscription.query.with_entities(Subscription.expiry_date).filter(Subscription.school_id==current_user.school_id).first()
     #convert Subscription.expiry_date value from the Subsciption table to date format
     if form.validate_on_submit():
         if form.picture.data:
