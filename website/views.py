8a9c3a6d156812e280be24512b8f5d92a0141552
Precious <precious.akam@hotmail.com>
2022-10-25 01:12:24 +1030

    Changes

--- website/views.py
+++ website/views.py
@@ -458,7 +458,7 @@ def managed_reviews():
         Employee['reviews'] = []
         # rev = SELECT distict date.posted FROM Response WHERE user.id = e.employee
         #SELECT DISTINCT response.date_posted FROM response LEFT JOIN evidence ON (evidence.user_id = response.user_id AND evidence.date_posted = response.date_posted) WHERE response.user_id = e.id AND evidence.title IS NULL;
-        rev = Response.query.with_entities(Response.date_posted).join(Evidence, Evidence.date_posted == Response.date_posted and Evidence.user_id == Response.user_id).join(Action, Action.date_posted == Response.date_posted and Action.user_id == Response.user_id).filter(Response.user_id == e.id).filter(Evidence.title == NULL).distinct(Response.date_posted).order_by(asc(Response.date_posted))
+        rev = Response.query.with_entities(Response.date_posted).join(Evidence, Evidence.date_posted == Response.date_posted and Evidence.user_id == Response.user_id).filter(Response.user_id == e.id).filter( Evidence.title == NULL).distinct(Response.date_posted).order_by(asc(Response.date_posted))
        
          #old one
         # rev = Response.query.with_entities(Response.date_posted).filter(Response.user_id == e.id).distinct(Response.date_posted).order_by(asc(Response.date_posted))
@@ -484,7 +484,7 @@ def managed_completed_reviews():
         Employee = {'id': e.id, 'name':name.first_name + " " + name.last_name, }
         Employee['reviews'] = []
         # rev = SELECT distict date.posted FROM Response WHERE user.id = e.employee
-        rev = Response.query.with_entities(Response.date_posted).join(Evidence, Evidence.date_posted == Response.date_posted and Evidence.user_id == Response.user_id).filter(Response.user_id == e.id).filter(Evidence.title == NULL).distinct(Response.date_posted).order_by(asc(Response.date_posted))
+        rev = Response.query.with_entities(Response.date_posted).join(Evidence, Evidence.date_posted == Response.date_posted and Evidence.user_id == Response.user_id).join(Action, Action.date_posted == Response.date_posted and Action.user_id == Action.date_posted).filter(Response.user_id == e.id).filter(Evidence.title == NULL and Action.title == NULL).distinct(Response.date_posted).order_by(asc(Response.date_posted))
         # old one
         # rev = Response.query.with_entities(Response.date_posted).filter(Response.user_id == e.id).distinct(Response.date_posted).order_by(asc(Response.date_posted))
         for r in rev:
